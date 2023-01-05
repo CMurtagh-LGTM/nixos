64a5cc112878f06d0dbbfff79d99e6e2b397bab5
CMurtagh-LGTM <cameron.murtagh00@gmail.com>
2023-01-05 18:14:06 +1100

    some zsh, pl10k no work

--- home.nix
+++ home.nix
@@ -32,7 +32,33 @@ in {
       enable = true;
       prezto = {
         enable = true;
+	pmodules = [
+	  "environment"
+	  "terminal"
+	  "editor"
+	  "history"
+	  "directory"
+	  "spectrum"
+	  "utility"
+ 	  "completion"
+  	  "prompt"
+	  "history-substring-search"
+	  "autosuggestions"
+	];
+	prompt.theme = "powerlevel10k";
+	ssh.identities = ["github"];
       };
+      plugins = [
+	{
+	  name = "per-directory-history";
+	  src = pkgs.fetchFromGitHub {
+	    owner = "jimhester";
+	    repo = "per-directory-history";
+	    rev = "master";
+	    sha256 = "sha256-7Z0qaDhgopKt9BDKSqdziw9jsVgiLLafs30wPPbz+oo=";
+	  };
+	}
+      ];
     };
   };
 }
